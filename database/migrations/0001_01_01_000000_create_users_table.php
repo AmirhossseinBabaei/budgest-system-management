953e67f446b37b144fac9c3e28a120c9ac5f1dfa
AmirhossseinBabaei <ab8622822@gmail.com>
2026-06-15 14:25:55 +0330

    add users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('phone', 20);
+            $table->string('avatar_src');
+            $table->string('job')->default('programmer');
+
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
             $table->timestamps();
         });
