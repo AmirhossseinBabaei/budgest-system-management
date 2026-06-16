3a9f827bcb5f705cedc9157313314825ce3b792f
AmirhossseinBabaei <ab8622822@gmail.com>
2026-06-16 17:43:58 +0330

    push api package

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('sanctum/register', [\App\Http\Controllers\Api\v1\Authentication\SanctumController::class, 'registerUser'])->name('sanctum.register');
+Route::post('sanctum/login', [\App\Http\Controllers\Api\v1\Authentication\SanctumController::class, 'LoginUser'])->name('sanctum.login');
+
+
