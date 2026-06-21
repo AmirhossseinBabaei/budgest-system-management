5f28b01736173370ac329ec9119ae23f9e7ea4e2
Javid Soltani <mojtabasultani82@gmail.com>
2026-06-21 16:19:28 +0330

    Login page added to Routes.jsx file

--- resources/react/React.js/src/routes/Routes.jsx
+++ resources/react/React.js/src/routes/Routes.jsx
@@ -1,7 +1,9 @@
 import Home from "../Pages/Home.jsx"
+import Login from "../Pages/Login/Login.jsx";
 
 const routes =[
-    {path:"/" ,element:<Home />}
+    {path:"/" ,element:<Home />},
+    {path:"/login" ,element:<Login />}
 ]
 
 export default routes ;
