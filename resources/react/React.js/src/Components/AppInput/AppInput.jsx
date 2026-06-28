7cafac3ec7dbb1722ba627a992029f65ee62317c
Javid Soltani <mojtabasultani82@gmail.com>
2026-06-28 20:43:06 +0330

    Renaming props in the AppInput component

--- resources/react/React.js/src/Components/AppInput/AppInput.jsx
+++ resources/react/React.js/src/Components/AppInput/AppInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Input({ titleLabel, textPlaceholder , typeInput , inputVal , chnageValue }) {
+export default function Input({ titleLabel, placeholder, type, inputVal, onChange, maxLength }) {
     return (
         <div className='flex flex-col gap-2'>
             <label htmlFor="" className=' font-semibold'>{titleLabel}</label>
-            <input type={typeInput} placeholder={textPlaceholder} className='text-sm border border-ColorBorder p-3 rounded-sm text-Secondary focus:border-Primary outline-Primary' value={inputVal} onChange={(e)=>chnageValue(e)} />
+            <input maxLength={maxLength} type={type} placeholder={placeholder} className='text-right text-sm border border-ColorBorder p-3 rounded-sm text-Secondary focus:border-Primary outline-Primary' value={inputVal} onChange={onChange} />
         </div>
     )
 }
